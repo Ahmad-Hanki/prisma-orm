645023615c1c97d8c7b1fda25e62c1f799b34f8a
ahmad <itxti909@gmail.com>
2024-03-02 10:59:26 +0300

    styled the actived link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { FaBug } from "react-icons/fa";
-
+import classnames from "classnames";
 const NavBar = () => {
+  const pathname = usePathname();
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -17,7 +21,9 @@ const NavBar = () => {
         {links.map((link) => {
           return (
             <Link
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={`${
+                link.href == pathname ? "text-zinc-900" : "text-zinc-500"
+              } hover:text-zinc-800 transition-colors`}
               key={link.href}
               href={link.href}
             >
